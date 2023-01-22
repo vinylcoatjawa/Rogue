8816fb5ada25f0b0d433464484496978b2f3094e
Jelci <andras3141592@gmail.com>
2023-01-22 22:58:23 +0100

    Add Player State event so that player can broadcast it state

---
+++ Assets/SO/SOEventSystem/Events/PlayerStateEvent.cs
@@ -0,0 +1,7 @@
+ using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Player State Event", menuName = "SO/Game Events/Player State Event")]
+/// <summary>
+/// Custom event for Player State type
+/// </summary>
+public class PlayerStateEvent : BaseGameEvent<PlayerBaseState> {}
